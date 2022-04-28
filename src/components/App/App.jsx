527d49f53e69894cf18b06c23327adbe6638ca5a
Fatuma Abdi <Abdi.fatuma776@gmail.com>
2022-04-28 18:34:25 -0500

    added GET Route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    getGallery();
+  }, [])
+const [Gallery,setGallery]=useState('')
+
+  const getGallery = () => {
+    axios({
+      method: 'GET',
+      url: '/gallery'
+    }).then((response) => {
+      console.log(response.data);
+      setGallery(response.data);
+    }).catch((error) => {
+      console.log('GET /gallery broke:', error);
+    })
+  }
+
+
+
+
+
+
+
+
     return (
       <div className="App">
         <header className="App-header">
